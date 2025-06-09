8b88fc6976002e1dbb461bb3133ea371c9b1cfa4
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2025-06-10 01:44:26 +0900

    docs(todo): update 문서 추가

--- src/main/java/com/example/webapp/controller/ToDoController.java
+++ src/main/java/com/example/webapp/controller/ToDoController.java
@@ -1,8 +1,11 @@
 package com.example.webapp.controller;
 
+import com.example.webapp.DTO.ToDoDTO;
 import com.example.webapp.entity.ApiResponse;
 import com.example.webapp.entity.ToDo;
 import com.example.webapp.service.ToDoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/todos")
 @RequiredArgsConstructor
+@Tag(name = "ToDo", description = "ToDo 관리 API")
 public class ToDoController {
 //    DI
     private final ToDoService toDoService;
@@ -38,9 +42,22 @@ public class ToDoController {
         return ResponseEntity.ok(response);
     }
 
-    @PatchMapping("/post")
-    public ResponseEntity<ToDo> update (@RequestBody ToDo toDo){
-        toDoService.updateToDo(toDo);
+    @Operation(
+            summary = "ToDo 업데이트",
+            description = """
+                    ## ToDo 업데이트 API
+                    
+                    기존의 ToDo를 업데이트 합니다.
+                    
+                    ### 필수 입력 항목
+                    - todo : 최대 50자
+                    - detail : 제한없음
+                    
+                    """
+    )
+    @PatchMapping("/{id}")
+    public ResponseEntity<ToDoDTO> update (@PathVariable Integer id, @RequestBody ToDoDTO toDo){
+        toDoService.updateToDo(id, toDo);
         return ResponseEntity.ok(toDo);
     }
 }

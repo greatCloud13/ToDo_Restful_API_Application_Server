7a5b2cd1f3b233a5309adbbeb7436b8d7df30bdd
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2025-09-04 22:35:15 +0900

    Refactor(Login): ToDo Entity 주석 수정

--- src/main/java/com/example/webapp/entity/ToDo.java
+++ src/main/java/com/example/webapp/entity/ToDo.java
@@ -14,17 +14,36 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ToDo {
-//    할일 ID
+
+
+    /**
+     * ToDoContext ID
+     *
+     */
     @Schema(description = "ToDo 게시물 ID", example = "1")
     private Integer id;
-//    할일
+
+    /**
+     * ToDoContext Title
+     */
     @Schema(description = "ToDo 제목", example = "마트 다녀오기", maxLength = 50)
     private String todo;
-//    할일 상세보기
+
+    /**
+     * ToDoContext Description
+     *
+     */
     @Schema(description = "ToDo 상세 내용", example = "돼지고기 500g 구매")
     private String detail;
-//    작성 일자
+
+    /**
+     * 작성 일자
+     */
     private LocalDateTime createdAt;
-//    업데이트 일자
+
+    /**
+     * 업데이트 일자
+     */
     private LocalDateTime updatedAt;
+
 }

d17ed8332728015e086b608bfe0c45ceb773b46e
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2025-09-07 23:50:06 +0900

    feat(Login): 테스트 코드 주석처리

--- src/main/java/com/example/webapp/WebappApplication.java
+++ src/main/java/com/example/webapp/WebappApplication.java
@@ -1,7 +1,6 @@
 package com.example.webapp;
 
 import com.example.webapp.entity.ToDo;
-import com.example.webapp.service.ToDoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,41 +16,41 @@ public class WebappApplication {
 	}
 
 //	DI
-	private final ToDoService service;
-
-	public void exe(){
-//		전체 검색
-		System.out.println("=== 전체 검색 ===");
-		for (ToDo row : service.findAllToDo()){
-			System.out.println(row);
-		}
-//		단일 검색
-		System.out.println("=== 단일 검색 ===");
-		System.out.println(service.findByIdToDo(1));
-
-//		단일 등록
-		ToDo todo = new ToDo();
-		todo.setTodo("리포지토리 테스트");
-		todo.setDetail("db 등록");
-		service.insertToDo(todo);
-		System.out.println("=== 등록확인 ===");
-		System.out.println(service.findByIdToDo(4));
-
-//		업데이트
-		ToDo target = service.findByIdToDo(4);
-		target.setTodo("리포지토리 테스트 업데이트");
-		target.setDetail("DB 업데이트");
-//		service.updateToDo(target);
-		System.out.println("=== 업데이트 확인 ===");
-		System.out.println(service.findByIdToDo(4));
-
-//		삭제
-		service.deleteToDo(4);
-		System.out.println("=== 삭제 확인 ===");
-		for(ToDo row : service.findAllToDo()){
-			System.out.println(row);
-		}
-
-	}
+//	private final ToDoService service;
+//
+//	public void exe(){
+////		전체 검색
+//		System.out.println("=== 전체 검색 ===");
+//		for (ToDo row : service.findAllToDo()){
+//			System.out.println(row);
+//		}
+////		단일 검색
+//		System.out.println("=== 단일 검색 ===");
+//		System.out.println(service.findByIdToDo(1));
+//
+////		단일 등록
+//		ToDo todo = new ToDo();
+//		todo.setTodo("리포지토리 테스트");
+//		todo.setDetail("db 등록");
+//		service.insertToDo(todo);
+//		System.out.println("=== 등록확인 ===");
+//		System.out.println(service.findByIdToDo(4));
+//
+////		업데이트
+//		ToDo target = service.findByIdToDo(4);
+//		target.setTodo("리포지토리 테스트 업데이트");
+//		target.setDetail("DB 업데이트");
+////		service.updateToDo(target);
+//		System.out.println("=== 업데이트 확인 ===");
+//		System.out.println(service.findByIdToDo(4));
+//
+////		삭제
+//		service.deleteToDo(4);
+//		System.out.println("=== 삭제 확인 ===");
+//		for(ToDo row : service.findAllToDo()){
+//			System.out.println(row);
+//		}
+//
+//	}
 
 }

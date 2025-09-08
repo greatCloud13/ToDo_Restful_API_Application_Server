f550cd58d79046a2847a916104b0d35c1eef5695
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2025-09-08 23:53:48 +0900

    feat(todo): todo 등록 DTO 구현

---
+++ src/main/java/com/example/webapp/DTO/TodoRequestDTO.java
@@ -0,0 +1,37 @@
+package com.example.webapp.DTO
+
+import com.example.webapp.entity.ToDo;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * 할 일 등록 DTO
+ */
+@Data
+@Builder
+public class TodoRequestDTO {
+
+    @Schema(description = "제목")
+    private String title;
+    
+    @Schema(description = "메모")
+    private String memo;
+
+    @Schema(description = "우선순위")
+    private ToDo.TaskPriority priority;
+
+    @Schema(description = "카테고리")
+    private String category;
+
+    @Schema(description = "상태")
+    private ToDo.TaskStatus status;
+
+    @Schema(description = "마감일")
+    private LocalDateTime targetDate;
+
+}

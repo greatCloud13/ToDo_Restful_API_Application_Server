4847f04d191b20a129414ec4ddacfe054801eed8
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2025-09-10 22:20:55 +0900

    FEAT(ToDo): ToDoService 인터페이스 구현
    - 상세 조회
    - 리스트 조회
    - 등록 구현

---
+++ src/main/java/com/example/webapp/service/impl/ToDoServiceImpl.java
@@ -0,0 +1,57 @@
+package com.example.webapp.service.impl;
+
+import com.example.webapp.DTO.TodoRequestDTO;
+import com.example.webapp.entity.ToDo;
+import com.example.webapp.entity.User;
+import com.example.webapp.repository.TodoRepository;
+import com.example.webapp.service.ToDoService;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ToDoServiceImpl implements ToDoService {
+
+    private final TodoRepository todoRepository;
+
+    @Override
+    public ToDo findTodoById(int id) {
+        log.info("할 일 조회 고유 id: {}",id);
+
+        return todoRepository.findById(id)
+                .orElseThrow(()->new EntityNotFoundException(
+                        "Not Found"
+                ));
+    }
+
+
+    @Override
+    public Page<ToDo> findTodoListByUser(User user, Pageable pageable) {
+        return todoRepository.findByUser(user, pageable);
+    }
+
+    @Override
+    public TodoRequestDTO saveToDo(TodoRequestDTO toDoDTO, User user) {
+
+        ToDo todo = ToDo.builder()
+                .title(toDoDTO.getTitle())
+                .memo(toDoDTO.getMemo())
+                .category(toDoDTO.getCategory())
+                .taskPriority(toDoDTO.getPriority())
+                .planningDate(toDoDTO.getTargetDate())
+                .status(toDoDTO.getStatus())
+                .user(user)
+                .build();
+
+        todoRepository.save(todo);
+
+        return toDoDTO;
+    }
+}

1c04d3ed1da586d475388099c61bb0be52c4f659
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2025-09-15 00:49:57 +0900

    doc, fix(todo): 주석 수정 및 기능 추가
    - 할 일 상태 변경 기능 추가
    - 할 일 삭제 기능 추가
    - 할 일 작성자 검증 기능 추가

--- src/main/java/com/example/webapp/service/ToDoService.java
+++ src/main/java/com/example/webapp/service/ToDoService.java
@@ -1,5 +1,6 @@
 package com.example.webapp.service;
 
+import com.example.webapp.DTO.ToDoResponseDTO;
 import com.example.webapp.DTO.TodoRequestDTO;
 import com.example.webapp.entity.ToDo;
 import com.example.webapp.entity.User;
@@ -11,34 +12,55 @@ import java.util.List;
 public interface ToDoService {
 
     /**
-     * id를 통해 할일 상세조회
+     * id를 통해 할 일 상세조회
      * @param id 할 일 고유 id
      * @return ToDo 객체
      */
-    public ToDo findTodoById(int id);
+    public ToDoResponseDTO findTodoById(int id, String username);
 
     /**
-     * User를 통해 할일 리스트 조회
+     * User를 통해 할 일 리스트 조회
      * @param user 사용자
      * @param pageable pageable 객체
      * @return 할 일 리스트
      */
-    public Page<ToDo> findTodoListByUser(User user, Pageable pageable);
+    public Page<ToDoResponseDTO> findTodoListByUser(User user, Pageable pageable);
 
     /**
      * 할 일 등록
-     * @param toDoDTO 할 일객체
+     * @param toDoDTO 할 일 객체
      * @return 등록된 할 일
      */
     public ToDo saveToDo(TodoRequestDTO toDoDTO, User user);
 
     /**
      * 할 일 수정
-     * @param id 할일 고유 id
+     * @param id 할 일 고유 id
      * @param updateToDo 수정할 할 일 객체
-     * @return 수정 결과
+     * @return 수정된 객체
      */
     public TodoRequestDTO UpdateToDo(int id, TodoRequestDTO updateToDo);
 
+    /**
+     * 할 일 상태 변경
+     * @param id 할 일 고유 id
+     * @param status 상태
+     * @return 수정된 객체
+     */
+    public ToDo.TaskStatus UpdateStatus(int id, ToDo.TaskStatus status);
+
+    /**
+     * 할 일 삭제
+     * @param id 할 일 고유 id
+     * @return 삭제 여부
+     */
+    public Boolean DeleteTodo(int id);
+
+    /**
+     * 할 일 작성자 검증
+     * @param user 사용자 객체
+     * @return 접근 가능 여부
+     */
+    public Boolean CheckOwner(int id, String username);
 
 }
--- src/main/java/com/example/webapp/service/impl/ToDoServiceImpl.java
+++ src/main/java/com/example/webapp/service/impl/ToDoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.webapp.service.impl;
 
+import com.example.webapp.DTO.ToDoResponseDTO;
 import com.example.webapp.DTO.TodoRequestDTO;
 import com.example.webapp.entity.ToDo;
 import com.example.webapp.entity.User;
@@ -10,7 +11,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -22,19 +25,25 @@ public class ToDoServiceImpl implements ToDoService {
     private final TodoRepository todoRepository;
 
     @Override
-    public ToDo findTodoById(int id) {
+    public ToDoResponseDTO findTodoById(int id, String username) {
         log.info("할 일 조회 고유 id: {}",id);
 
-        return todoRepository.findById(id)
-                .orElseThrow(()->new EntityNotFoundException(
-                        "Not Found"
+        ToDo todo = todoRepository.findById(id)
+                .orElseThrow(()->new AccessDeniedException(
+                        "권한이 없거나 존재하지 않는 게시물입니다."
                 ));
+
+
+        return ToDoResponseDTO.from(todo);
     }
 
-
     @Override
-    public Page<ToDo> findTodoListByUser(User user, Pageable pageable) {
-        return todoRepository.findByUser(user, pageable);
+    @Transactional(readOnly = true)
+    public Page<ToDoResponseDTO> findTodoListByUser(User user, Pageable pageable) {
+
+        Page<ToDo> result = todoRepository.findByUser(user, pageable);
+
+        return result.map(ToDoResponseDTO :: from);
     }
 
     @Override
@@ -69,4 +78,47 @@ public class ToDoServiceImpl implements ToDoService {
         todoRepository.save(todo);
         return updateToDo;
     }
+
+    @Override
+    public ToDo.TaskStatus UpdateStatus(int id, ToDo.TaskStatus status) {
+
+        ToDo todo = todoRepository.findById(id)
+                .orElseThrow(
+                        ()-> new EntityNotFoundException(
+                                "Not Found"
+                        )
+                );
+
+        todo.setStatus(status);
+
+        ToDo result = todoRepository.save(todo);
+
+        return result.getStatus();
+    }
+
+    @Override
+    public Boolean DeleteTodo(int id) {
+
+        ToDo todo = todoRepository.findById(id).orElseThrow(
+                ()-> new EntityNotFoundException(
+                        "Not Found"
+                ));
+
+        todoRepository.delete(todo);
+
+        return true;
+    }
+
+    @Override
+    public Boolean CheckOwner(int id, String username) {
+
+        ToDo todo = todoRepository.findById(id).orElseThrow(
+                ()->new EntityNotFoundException(
+                        "Not Found"
+                ));
+
+        return username.equals(todo.getUser().getUsername());
+    }
+
+
 }

8d5ba6d6f486b8acddea54c536c064e214de7114
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2025-09-15 00:49:01 +0900

    fix(todo): ToDoResponseDTO 역직렬화 버그 수정및 사용자 변수 추가, ToDO 생성 정적 팩토리 메서드 추가

--- src/main/java/com/example/webapp/DTO/ToDoResponseDTO.java
+++ src/main/java/com/example/webapp/DTO/ToDoResponseDTO.java
@@ -1,6 +1,12 @@
 package com.example.webapp.DTO;
 
 import com.example.webapp.entity.ToDo;
+import com.example.webapp.entity.User;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -36,6 +42,41 @@ public class ToDoResponseDTO {
     @Schema(description = "카테고리")
     private String category;
 
+    @Schema(description = "마감일")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime planningDate;
+
+    @Schema(description = "종료일")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime doneAt;
+
     @Schema(description = "생성일자")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime createdAt;
+
+    @Schema(description = "사용자")
+    private String username;
+
+
+    public static ToDoResponseDTO from(ToDo todo) {
+        return ToDoResponseDTO.builder()
+                .id(todo.getId())
+                .title(todo.getTitle())
+                .memo(todo.getMemo())
+                .taskStatus(todo.getStatus())
+                .taskPriority(todo.getTaskPriority())
+                .planningDate(todo.getPlanningDate())
+                .doneAt(todo.getDoneAt())
+                .category(todo.getCategory())
+                .createdAt(todo.getCreatedAt())
+                .username(todo.getUser().getUsername())
+                .build();
+    }
+
 }

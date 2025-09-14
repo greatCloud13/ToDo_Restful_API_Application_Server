d6b5285fc0c858337d58c574f20e8cf134413c2a
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2025-09-15 00:47:18 +0900

    fix(todo): N+1, Lazy 로딩으로 인한 버그 수정을 위한 FETCH 쿼리 추가

--- src/main/java/com/example/webapp/repository/TodoRepository.java
+++ src/main/java/com/example/webapp/repository/TodoRepository.java
@@ -5,6 +5,8 @@ import com.example.webapp.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,5 +25,7 @@ public interface TodoRepository extends JpaRepository<ToDo, Long> {
      * @param user User entity
      * @return 페이징 된 할 일 리스트
      */
-    Page<ToDo> findByUser(User user, Pageable pageable);
+    @Query("SELECT t FROM ToDo t JOIN FETCH t.user WHERE t.user = :user")
+    Page<ToDo> findByUser(@Param("user")User user, Pageable pageable);
+
 }

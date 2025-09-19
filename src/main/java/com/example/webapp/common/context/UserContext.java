2fb0f9a8109ccd3aa2525455b61cabfb2a9d1775
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2025-09-19 19:34:49 +0900

    FEAT(AOP): UserContext 객체 구현

---
+++ src/main/java/com/example/webapp/common/context/UserContext.java
@@ -0,0 +1,23 @@
+package com.example.webapp.common.context;
+
+import com.example.webapp.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserContext {
+
+    private static final ThreadLocal<User> currentUser = new ThreadLocal<>();
+
+    public static void setCurrentUser(User user){
+        currentUser.set(user);
+    }
+
+    public static User getCurrentUser(){
+        return currentUser.get();
+    }
+
+    public static void clear(){
+        currentUser.remove();
+    }
+
+}

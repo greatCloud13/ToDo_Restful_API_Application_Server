5d0abdac88136cac3cb232818510e8e0a7a81447
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2025-09-20 23:07:27 +0900

    REFACTOR(Dashboard): DashboardController 사용하지 않는 import 정리

--- src/main/java/com/example/webapp/controller/DashboardController.java
+++ src/main/java/com/example/webapp/controller/DashboardController.java
@@ -12,9 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController

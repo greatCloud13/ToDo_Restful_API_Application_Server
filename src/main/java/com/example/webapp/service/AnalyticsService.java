2c465b82ade7b7ca8b1e1a8a6386fc3264ee798e
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2025-09-25 21:47:25 +0900

    FEAT(Analytics): AnalyticsService 일별 완료 추이 Interface 작성 및 구현

--- src/main/java/com/example/webapp/service/AnalyticsService.java
+++ src/main/java/com/example/webapp/service/AnalyticsService.java
@@ -1,9 +1,15 @@
 package com.example.webapp.service;
 
+import com.example.webapp.DTO.AnalyticsTrendDTO;
 import com.example.webapp.DTO.SummaryDTO;
+import com.example.webapp.DTO.request.AnalyticsTrendRequestDTO;
 import com.example.webapp.DTO.request.SummaryRequestDTO;
+
+import java.util.List;
 
 public interface AnalyticsService {
 
     SummaryDTO calcSummary(SummaryRequestDTO requestDTO);
-}
+
+    List<AnalyticsTrendDTO> getWeekTrend(String trendPeriod);
+    }
--- src/main/java/com/example/webapp/service/impl/AnalyticsServiceImpl.java
+++ src/main/java/com/example/webapp/service/impl/AnalyticsServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.webapp.service.impl;
 
+import com.example.webapp.DTO.AnalyticsTrendDTO;
 import com.example.webapp.DTO.SummaryDTO;
+import com.example.webapp.DTO.request.AnalyticsTrendRequestDTO;
 import com.example.webapp.DTO.request.SummaryRequestDTO;
 import com.example.webapp.common.annotations.InjectUserEntity;
 import com.example.webapp.common.context.UserContext;
@@ -9,14 +11,19 @@ import com.example.webapp.entity.User;
 import com.example.webapp.repository.TodoRepository;
 import com.example.webapp.service.AnalyticsService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AnalyticsServiceImpl implements AnalyticsService {
 
     private final TodoRepository todoRepository;
@@ -43,12 +50,53 @@ public class AnalyticsServiceImpl implements AnalyticsService {
         result.setCompleted(completed);
         result.setPending(pending);
         result.setInProgress(inProgress);
-        result.setCompletionRate(completed*100/completed);
+        result.setCompletionRate(completed * 100 / completed);
         result.setOverdueCount(todoRepository.countTodosAfterPlanningDate(LocalDateTime.now(), user.getId()));
         result.setUrgentCount(todoRepository.countByUserAndTaskPriority(user, ToDo.TaskPriority.VERY_HIGH));
         result.setActiveDays(todoRepository.countActiveDays(user.getId(), LocalDateTime.now()));
 
         return result;
 
+    }
+
+    @InjectUserEntity
+    @Transactional(readOnly = true)
+    @Override
+    public List<AnalyticsTrendDTO> getWeekTrend(String trendPeriod) {
+
+        User user = UserContext.getCurrentUser();
+
+        List<AnalyticsTrendDTO> result = new ArrayList<>();
+
+        LocalDate now = LocalDate.now();
+
+        for(int i=6; i>=0; i--){
+            AnalyticsTrendDTO trend = new AnalyticsTrendDTO();
+            LocalDate targetDate;
+            long total;
+            long completed;
+
+            targetDate = now.minusDays(i);
+            log.info("조회 일자:{}", targetDate);
+            LocalDateTime startDate = targetDate.atTime(LocalTime.MIN);
+            LocalDateTime endDate = targetDate.atTime(LocalTime.MAX);
+
+            total = todoRepository.countByUserAndCreatedAtBefore(user, targetDate.atTime(LocalTime.MAX));
+            log.info("조회 일자까지의 총 할 일 갯수: {}", total);
+
+            completed = todoRepository.countByUserAndCreatedAtBeforeAndStatus(user, targetDate.atTime(LocalTime.MAX), ToDo.TaskStatus.COMPLETE);
+            log.info("조회 일자까지 완료한 할 일 갯수: {}", completed);
+
+            trend.setDate(targetDate);
+            trend.setCompleted(completed);
+            trend.setTotal(total);
+            trend.setCompletionRate(completed*100/total);
+            trend.setCreated(todoRepository.countByUserAndCreatedAtBetween(user, startDate, endDate));
+
+            result.add(trend);
+
+        }
+
+        return result;
     }
 }

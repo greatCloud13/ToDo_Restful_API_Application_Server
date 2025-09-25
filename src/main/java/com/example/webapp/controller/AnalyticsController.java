e018b9baca2eb43eb2b86df5fcf472add225b654
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2025-09-25 21:46:23 +0900

    FEAT(Analytics): AnalyticsController 일별 완료 추이 요청 API 구현

---
+++ src/main/java/com/example/webapp/controller/AnalyticsController.java
@@ -0,0 +1,64 @@
+package com.example.webapp.controller;
+
+import com.example.webapp.DTO.AnalyticsTrendDTO;
+import com.example.webapp.DTO.SummaryDTO;
+import com.example.webapp.DTO.request.AnalyticsTrendRequestDTO;
+import com.example.webapp.DTO.request.SummaryRequestDTO;
+import com.example.webapp.service.AnalyticsService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/analytics")
+@RequiredArgsConstructor
+public class AnalyticsController {
+
+    private final AnalyticsService analyticsService;
+
+    @Operation(
+            summary = "통계 요약 요청 API",
+            description = """
+                    ## 통계 요약 요청 API
+                    선택한 기간의 통계 요약을 조회합니다.
+                    - 개발일자:
+                    - 수정일자: .
+                    - 테스트 여부:
+
+                    ### 필수 입력 항목
+                    """
+    )
+    @GetMapping("/summary")
+    public ResponseEntity<SummaryDTO> summary(SummaryRequestDTO request){
+
+        SummaryDTO result = analyticsService.calcSummary(request);
+
+        return ResponseEntity.ok(result);
+
+    }
+
+    @Operation(
+            summary = "일별 완료 추이 요청 API",
+            description = """
+                    ## 일별 완료 추이 요청 API
+                    일주일 간의 완료 추이를 요청합니다.
+                    - 개발일자:
+                    - 수정일자: .
+                    - 테스트 여부:
+
+                    ### 필수 입력 항목
+                    """
+    )
+    @GetMapping("/trend")
+    public ResponseEntity<List<AnalyticsTrendDTO>> trend(@RequestParam("period") String trendPeriod){
+
+        List<AnalyticsTrendDTO> result = analyticsService.getWeekTrend(trendPeriod);
+
+        return ResponseEntity.ok(result);
+    }
+
+
+}

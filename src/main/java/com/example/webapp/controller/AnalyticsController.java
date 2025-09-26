14a70215cd147ed8d7d98224aaed355ccc4f098c
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2025-09-26 21:01:48 +0900

    FEAT(Analytics): 분포 분석 API 추가

--- src/main/java/com/example/webapp/controller/AnalyticsController.java
+++ src/main/java/com/example/webapp/controller/AnalyticsController.java
@@ -1,5 +1,6 @@
 package com.example.webapp.controller;
 
+import com.example.webapp.DTO.AnalyticsDistributionDTO;
 import com.example.webapp.DTO.AnalyticsTrendDTO;
 import com.example.webapp.DTO.SummaryDTO;
 import com.example.webapp.DTO.request.AnalyticsTrendRequestDTO;
@@ -59,6 +60,27 @@ public class AnalyticsController {
 
         return ResponseEntity.ok(result);
     }
+
+    @Operation(
+            summary = " 분포 분석 요청 API",
+            description = """
+                    ## 분포 분석 요청 API
+                    분포분석 그래프에 필요한 데이터를 요청합니다.
+                    - 개발일자:
+                    - 수정일자: .
+                    - 테스트 여부:
+
+                    ### 필수 입력 항목
+                    """
+    )
+    @GetMapping("/distribution")
+    public ResponseEntity<AnalyticsDistributionDTO> distribution(@RequestParam("period") String trendPeriod){
+
+        AnalyticsDistributionDTO result = new AnalyticsDistributionDTO();
+
+        return ResponseEntity.ok(result);
+    }
+
 
 
 }

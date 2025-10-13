22d58a5c8587939d4c301656d2139a21da0e0fd5
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2025-10-13 22:22:13 +0900

    FEAT(ToDo): ToDoService 일괄 상태 변경 인터페이스 정의 및 구현

--- src/main/java/com/example/webapp/service/ToDoService.java
+++ src/main/java/com/example/webapp/service/ToDoService.java
@@ -2,6 +2,7 @@ package com.example.webapp.service;
 
 import com.example.webapp.DTO.ToDoResponseDTO;
 import com.example.webapp.DTO.TodoRequestDTO;
+import com.example.webapp.DTO.request.BulkUpdateRequest;
 import com.example.webapp.entity.ToDo;
 import com.example.webapp.entity.User;
 import org.springframework.data.domain.Page;
@@ -62,5 +63,12 @@ public interface ToDoService {
      * @return 접근 가능 여부
      */
     public Boolean CheckOwner(int id, String username);
+
+    /**
+     * 요청한 ID의 할 일 들의 상태를 일괄 변경합니다
+     * @param request BulkUpdateRequest 객체
+     * @return 변경된 ToDoResponseDTO 객체 리스트
+     */
+    public List<ToDoResponseDTO> bulkUpdate(BulkUpdateRequest request);
 
 }

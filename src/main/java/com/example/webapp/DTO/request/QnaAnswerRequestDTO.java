f4956d9b616dccee413db8cdc719d2399c98dec2
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2025-10-16 00:43:25 +0900

    FEAT(QNA): Qna 답변 응답 DTO 구현

---
+++ src/main/java/com/example/webapp/DTO/request/QnaAnswerRequestDTO.java
@@ -0,0 +1,15 @@
+package com.example.webapp.DTO.request;
+
+public class QnaAnswerRequestDTO {
+
+    /**
+     * 문의 ID
+     */
+    private Long id;
+
+    /**
+     * 답변 내용
+     */
+    private String answer;
+
+}

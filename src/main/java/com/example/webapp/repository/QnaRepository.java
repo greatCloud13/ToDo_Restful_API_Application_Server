55d86f5fb9e196f7942e92a7fc284217899fb48a
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2025-10-16 00:44:07 +0900

    FEAT(QNA): Qna Repository 구현 및 리스토 조회 구현

---
+++ src/main/java/com/example/webapp/repository/QnaRepository.java
@@ -0,0 +1,21 @@
+package com.example.webapp.repository;
+
+import com.example.webapp.entity.Qna;
+import com.example.webapp.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Repository
+public interface QnaRepository extends JpaRepository<Qna, Long> {
+
+    @Query("SELECT q FROM Qna q JOIN FETCH q.owner WHERE q.owner = :owner")
+    Page<Qna> findAllByOwner(@Param("owner") User owner, Pageable pageable);
+
+}

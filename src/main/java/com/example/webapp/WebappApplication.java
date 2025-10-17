ee5dd8e329ce1f21d003e68042132874d8d587a9
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2025-10-17 23:02:20 +0900

    FEAT(Project): 테스트용 주석 코드 제거

--- src/main/java/com/example/webapp/WebappApplication.java
+++ src/main/java/com/example/webapp/WebappApplication.java
@@ -13,44 +13,10 @@ public class WebappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebappApplication.class, args);
+
+
+
 	}
 
-//	DI
-//	private final ToDoService service;
-//
-//	public void exe(){
-////		전체 검색
-//		System.out.println("=== 전체 검색 ===");
-//		for (ToDo row : service.findAllToDo()){
-//			System.out.println(row);
-//		}
-////		단일 검색
-//		System.out.println("=== 단일 검색 ===");
-//		System.out.println(service.findByIdToDo(1));
-//
-////		단일 등록
-//		ToDo todo = new ToDo();
-//		todo.setTodo("리포지토리 테스트");
-//		todo.setDetail("db 등록");
-//		service.insertToDo(todo);
-//		System.out.println("=== 등록확인 ===");
-//		System.out.println(service.findByIdToDo(4));
-//
-////		업데이트
-//		ToDo target = service.findByIdToDo(4);
-//		target.setTodo("리포지토리 테스트 업데이트");
-//		target.setDetail("DB 업데이트");
-////		service.updateToDo(target);
-//		System.out.println("=== 업데이트 확인 ===");
-//		System.out.println(service.findByIdToDo(4));
-//
-////		삭제
-//		service.deleteToDo(4);
-//		System.out.println("=== 삭제 확인 ===");
-//		for(ToDo row : service.findAllToDo()){
-//			System.out.println(row);
-//		}
-//
-//	}
 
 }
